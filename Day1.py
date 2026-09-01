3216c5b035ba60092bee6f3a9677e7bfcb3bbe2c
Sreeraj SP <117434811+iamsreeraj@users.noreply.github.com>
2026-09-01 16:58:05 +0530

    Add Python Day 1 practice
    Practiced Python basics, variables, data types, strings, booleans, operators, f-strings, and basic expressions.

---
+++ Day1.py
@@ -0,0 +1,11 @@
+Name = 'Sreeraj sp'
+Age = 22
+Course = 'Python'
+Marks = 85
+print(f"My name is {Name}, i am {Age} years old and i am learning {Course}.I got {Marks} in it ")
+print(Marks > 50)
+print(bool( Name))
+print(bool(0))
+Marks += 10
+print(Marks)
+print(Age == 18)
