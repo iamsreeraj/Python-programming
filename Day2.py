c66528727523599bcf1eb2a3d8d4488716765d24
Sreeraj SP <117434811+iamsreeraj@users.noreply.github.com>
2026-09-02 17:54:51 +0530

    Python Day 2 practice
    Practiced Python lists, including accessing, changing, adding, removing, and checking list items.

---
+++ Day2.py
@@ -0,0 +1,14 @@
+Students = ['Sreeraj','Mushroof','Hani','Jihan','Mishal']
+print(Students)
+print(Students[0])
+print(Students[4])
+Students[2]='Rahul'
+Students.append("Anu")
+Students.pop(0)
+print(Students)
+print(len(Students))
+if 'Rahul' in Students:
+    print("Rahul is in the list")
+else:
+    print("Rahul is not in the list")    
+ 
