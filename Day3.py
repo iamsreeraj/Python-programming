bff689b58974b41983ce4e31fca6eb7b97f99f52
Sreeraj SP <117434811+iamsreeraj@users.noreply.github.com>
2026-09-03 18:12:23 +0530

    Python Day 3 practice
    Practiced Python lists, tuples, sets, and dictionaries, including list operations, tuple unpacking, set union and intersection, and adding dictionary items.

---
+++ Day3.py
@@ -0,0 +1,21 @@
+student_names = ["Arthur","John","Dutch","Micah","Bill"]
+print(student_names)
+student_names.append("Hossea")
+student_names.remove("Micah")
+print(student_names)
+subject = ("maths","history","science")
+(a,b,c) = subject
+print(a)
+print(b)
+print(c)
+food = {"rice","curry","biriyani","sandwich"}
+snacks = {"chips","pups","sandwich"}
+print(food.union(snacks))
+x = food.intersection(snacks)
+print(x)
+print(food)
+mates = { "name":"sreeraj",
+          "age":22,
+          "Course":"data"}
+mates["status"] ="good"
+print(mates)
